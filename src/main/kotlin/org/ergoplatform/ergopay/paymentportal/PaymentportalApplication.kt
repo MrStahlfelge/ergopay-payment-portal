59d8ac806146855fedcf702903849d434ef0a10f
Benjamin Schulte <bs@golfgl.de>
2022-06-15 10:47:53 +0200

    Generated by Spring Initializr

---
+++ src/main/kotlin/org/ergoplatform/ergopay/paymentportal/PaymentportalApplication.kt
@@ -0,0 +1,11 @@
+package org.ergoplatform.ergopay.paymentportal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentportalApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaymentportalApplication>(*args)
+}
---
+++ src/test/kotlin/org/ergoplatform/ergopay/paymentportal/PaymentportalApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ergoplatform.ergopay.paymentportal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentportalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
